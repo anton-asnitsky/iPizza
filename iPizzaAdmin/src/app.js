95cc2149e5e0299197f1079948976a9e60d759fb
Anton Asnitsky <anton.asnitsky@gmail.com>
2018-09-16 21:38:24 +0300

    Added store files, removed unused dependencies

--- iPizzaAdmin/src/app.js
+++ iPizzaAdmin/src/app.js
@@ -2,15 +2,10 @@ import React                from 'react';
 import ReactDOM             from 'react-dom';
 import {Provider}           from 'react-redux';
 import AppRouter            from './routers/AppRouter';   
-import { setTextFilter }    from './actions/filters';
-import { addExpense }       from './actions/expenses';
 import configureStore       from './store/configure-store';
-
-import './firebase/firebase';
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
-import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 const jsx = (
